b9421d48b585515153e77c4f40418d890cd426b7
Anthony <anthony@smokeci.com>
2019-12-02 16:11:56 +0200

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './services/user/auth.guard';
 
 const routes: Routes = [
   {
@@ -9,11 +10,28 @@ const routes: Routes = [
   },
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule),
+    canActivate: [AuthGuard],
   },
   {
     path: 'list',
-    loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
+    loadChildren: () => import('./list/list.module').then(m => m.ListPageModule),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./pages/signup/signup.module').then( m => m.SignupPageModule),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule),
+    canActivate: [AuthGuard],
   }
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-
+import { Plugins } from '@capacitor/core';
 import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import * as firebase from 'firebase/app';
+
+const { SplashScreen, StatusBar } = Plugins;
 
 @Component({
   selector: 'app-root',
@@ -23,18 +24,29 @@ export class AppComponent {
     }
   ];
 
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
+  constructor(private platform: Platform) {
     this.initializeApp();
+    var firebaseConfig = {
+      apiKey: "AIzaSyC03ViKKt51H5amA6WKXveEw2zqikKF0cU",
+      authDomain: "kofi-a0ba2.firebaseapp.com",
+      databaseURL: "https://kofi-a0ba2.firebaseio.com",
+      projectId: "kofi-a0ba2",
+      storageBucket: "kofi-a0ba2.appspot.com",
+      messagingSenderId: "625510210314",
+      appId: "1:625510210314:web:e42058a7da6c47541878f3",
+      measurementId: "G-Y2E297KS78"
+    };
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
   }
 
   initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+    SplashScreen.hide().catch(error => { 
+      console.error(error);
+    });
+
+    StatusBar.hide().catch(error => {
+      console.error(error);
     });
   }
 }
