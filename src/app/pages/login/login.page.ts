4ca488047d6eda4c9e3ff602098d80cdc4a74a2d
Anthony <anthony@smokeci.com>
2019-12-04 16:49:23 +0200

    Added login page form and background as well as basic validation

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -9,6 +9,20 @@ import { Validators, FormBuilder, FormControl } from '@angular/forms';
 })
 export class LoginPage implements OnInit {
   loginForm : FormGroup;
+  errorMessages = {
+    'email':[
+      {type:'required', message:'Email is required...'},
+      {type:'minLength', message:'Email length must be longer than or equal to 6 characters'},
+      {type:'maxLength', message:'Email length must be no longer than 50 characters'},
+      {type:'pattern', message:'Please enter a valid email address...'}
+    ],
+    'password':[
+      {type:'required', message:'Password is required...'},
+      {type:'minLength', message:'Password length must be longer than or equal to 6 characters'},
+      {type:'maxLength', message:'Password length must be no longer than 50 characters'}
+    ]
+  }
+
   constructor(
     public formBuilder: FormBuilder
   ){
@@ -32,6 +46,6 @@ export class LoginPage implements OnInit {
     
   }
   LoginTrigger(){
-    console.log("Submitted");
+  
   }
 }
